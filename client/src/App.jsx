eaba37de12cd93c6b219bd339dd5cae1ea49dcbb
Filippo Vigani <filippo@clairy.com>
2019-06-04 22:04:43 +0200

    Create appointments UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,41 @@
-import React, { Component } from 'react';
-import { hot } from 'react-hot-loader/root';
-import Todos from './components/todo-box';
+import React, {Component} from 'react'
+import {hot} from 'react-hot-loader/root'
+import * as PropTypes from 'prop-types'
+import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles'
+import {blue} from '@material-ui/core/colors'
+import Demo from './components/appointments'
+
+const lightTheme = createMuiTheme({
+	palette: {
+		type: 'light',
+		primary: blue,
+	},
+	typography: {
+		useNextVariants: true,
+	},
+})
+
+const ThemeContainer = ({theme, children}) => (
+	<MuiThemeProvider theme={theme}>
+		{children}
+	</MuiThemeProvider>
+)
+
+ThemeContainer.propTypes = {
+	theme: PropTypes.any.isRequired,
+	children: PropTypes.node.isRequired,
+}
+
 
 class App extends Component {
 	constructor(props) {
-		super(props);
-		this.state = {};
+		super(props)
+		this.state = {}
 	}
 
 	render() {
-		return <Todos title="Hello Listen js" />;
+		return <ThemeContainer theme={lightTheme}><Demo /></ThemeContainer>
 	}
 }
 
-export default hot(App);
+export default hot(App)
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+	<React.StrictMode><App /></React.StrictMode>
+	, document.getElementById('app'));
